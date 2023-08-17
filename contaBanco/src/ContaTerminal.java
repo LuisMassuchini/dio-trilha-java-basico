fa3a97e43da2bc92bc4e42f82e9ff7ec5805f7ed
Luís Massuchini <luis.massuchini@gmail.com>
2023-08-17 09:55:30 -0300

    finaliznado contaTerminal

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Banco da Fortuna, siga os passos do programa para abertura da conta.");
+        System.out.println("Qual é o seu nome? ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o número da agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da conta:");
+        int conta = scanner.nextInt();
+
+        System.out.println("Saldo inicial:");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
